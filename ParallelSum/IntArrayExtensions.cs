7457ee8729124672685671697191e1d6f5eef99d
an.morozova <42morozova.a@gmail.com>
2023-09-28 22:35:26 +0300

    Add project files.

---
+++ ParallelSum/IntArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace ParallelSum
+{
+	public static class IntArrayExtensions
+	{
+		public static int GetSumSimple(this int[] array)
+		{
+			return array.Sum();
+		}
+
+		public static int GetSumParallel(this int[] array)
+		{
+			int sum = 0;
+			Parallel.ForEach(array, i => Interlocked.Add(ref sum, i));
+			return sum;
+		}
+
+		public static int GetSumPLINQ(this int[] array)
+		{
+			return array.AsParallel().Sum();
+		}
+	}
+}
---
+++ ParallelSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using ParallelSum;
+using System.Diagnostics;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int[] ArrayS = new int[100000].Select(x => Random.Shared.Next(0, 100)).ToArray();
+		int[] ArrayM = new int[1000000].Select(x => Random.Shared.Next(0, 100)).ToArray();
+		int[] ArrayL = new int[10000000].Select(x => Random.Shared.Next(0, 100)).ToArray();
+
+		Console.WriteLine("Start calculating for ArrayS");
+		CalculateAndPrint(ArrayS);
+		Console.WriteLine();
+
+		Console.WriteLine("Start calculating for ArrayM");
+		CalculateAndPrint(ArrayM);
+		Console.WriteLine();
+
+		Console.WriteLine("Start calculating for ArrayL");
+		CalculateAndPrint(ArrayL);
+	}
+
+	private static void CalculateAndPrint(int[] array)
+	{
+		Stopwatch sw = Stopwatch.StartNew();
+		var sumOfArraySimple = array.GetSumSimple();
+		Console.WriteLine($"For {array.Length} array sumOfArraySimple = {sumOfArraySimple} in {sw.ElapsedMilliseconds} ms");
+
+		sw.Restart();
+		var sumOfArrayParallel = array.GetSumParallel();
+		Console.WriteLine($"For {array.Length} array sumOfArrayParallel = {sumOfArrayParallel} in {sw.ElapsedMilliseconds} ms");
+
+		sw.Restart();
+		var sumOfArrayPLINQ = array.GetSumPLINQ();
+		Console.WriteLine($"For {array.Length} array sumOfArrayPLINQ = {sumOfArrayPLINQ} in {sw.ElapsedMilliseconds} ms");
+
+		sw.Stop();
+	}
+}
