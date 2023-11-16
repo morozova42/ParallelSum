464a21541396f7e34de26e9113e86a6a85d7c2e6
An.Morozova <predator1435@gmail.com>
2023-11-16 20:10:09 +0300

    trying with threads

--- ParallelSum/IntArrayExtensions.cs
+++ ParallelSum/IntArrayExtensions.cs
@@ -2,6 +2,16 @@
 {
 	public static class IntArrayExtensions
 	{
+		private static int sum;
+		private static void ChunkSum(object? obj)
+		{
+			int[] array = obj as int[];
+			for (int i = 0; i < array.Length; i++)
+			{
+				Interlocked.Add(ref sum, array[i]);
+			}
+		}
+
 		public static int GetSumSimple(this int[] array)
 		{
 			return array.Sum();
@@ -9,8 +19,18 @@
 
 		public static int GetSumParallel(this int[] array)
 		{
-			int sum = 0;
-			Parallel.ForEach(array, i => Interlocked.Add(ref sum, i));
+			List<Thread> threads = new List<Thread>();
+			int chunkSize = array.Length / 10;
+			var chunks = array.Chunk(chunkSize);
+
+			for (int i = 0; i < 10; i++)
+			{
+				threads.Add(new Thread(ChunkSum));
+			}
+			for (int i = 0; i < threads.Count; i++)
+			{
+				threads[i].Start(chunks.ElementAt(i));
+			}
 			return sum;
 		}
 
